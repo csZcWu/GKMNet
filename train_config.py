05304e5bd47065ad73e2c49dd22f3c126cdff727
csZcWu <93129292+csZcWu@users.noreply.github.com>
2022-11-28 11:40:23 +0800

    Update train_config.py

--- train_config.py
+++ train_config.py
@@ -1,12 +1,12 @@
 # train_config
 test_time = False
 train = {}
-train['train_img_path'] = '/dataset/dd_dp_dataset_png/train_c/source'
-train['train_gt_path'] = '/dataset/dd_dp_dataset_png/train_c/target'
-train['val_img_path'] = '/dataset/dd_dp_dataset_png/val_c/source'
-train['val_gt_path'] = '/dataset/dd_dp_dataset_png/val_c/target'
-train['test_img_path'] = '/dataset/dd_dp_dataset_png/test_c/source'
-train['test_gt_path'] = '/dataset/dd_dp_dataset_png/test_c/target'
+train['train_img_path'] = 'D:/wzc/dd_dp_dataset_png/train_c/source'
+train['train_gt_path'] = 'D:/wzc/dd_dp_dataset_png/train_c/target'
+train['val_img_path'] = 'D:/wzc/dd_dp_dataset_png/val_c/source'
+train['val_gt_path'] = 'D:/wzc/dd_dp_dataset_png/val_c/target'
+train['test_img_path'] = 'D:/wzc/dd_dp_dataset_png/test_c/source'
+train['test_gt_path'] = 'D:/wzc/dd_dp_dataset_png/test_c/target'
 train['batch_size'] = 4
 train['val_batch_size'] = 4
 train['test_batch_size'] = 1
@@ -21,9 +21,9 @@ train['nesterov'] = True
 
 # config for save , log and resume
 train['sub_dir'] = 'checkpoints'
-train['resume'] = './checkpoints'
-train['resume_epoch'] = 3818  # None means the last epoch
-train['resume_optimizer'] = './checkpoints'
+train['resume'] = './save/checkpoints/0'
+train['resume_epoch'] = None  # None means the last epoch
+train['resume_optimizer'] = './save/checkpoints/0'
 
 net = {}
 net['xavier_init_all'] = True
